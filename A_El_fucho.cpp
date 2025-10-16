af0612376bf96d2117f92cbdfddae081f059344b
WOLV1E <ahsananjum1247@gmail.com>
2025-10-16 21:53:35 +0600

    Add files via upload

---
+++ A_El_fucho.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+void allahbhalojanen(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+int main(){  
+    allahbhalojanen();
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        cout<<2*(n-1)<<endl;
+    }
+    return 0;
+}
